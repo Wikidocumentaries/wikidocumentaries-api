1821020d683d37d1c02cb8e402e49c70294fdba9
Susanna Ånäs <susanna.anas@gmail.com>
2019-06-09 12:15:20 +0300

    Update dpla.js

--- dpla.js
+++ dpla.js
@@ -31,66 +31,100 @@ module.exports = {
             var image = {
                 id: item.id,
                 source: 'DPLA',
-                title: [], //dcTitleLangAware
-                description: '',//dcDescriptionLangAware/langcode
-                imageURL: '',
+                // provider: dataProvider,
+                title: [],
+                description: '',
+                // imageURL: '',
                 thumbURL: item.object,
-                download_url: '',
+                // download_url: '',
                 creators: [],
                 institutions: [],
-                subjects: '',
-                // //legacy_tags: result.legacy_tags,
-                // //license: result.license,
-                // //license_id: result.license, //rights
-                // //license_version: result.license_version,
-                license_link: '',
-                infoURL: '',
-                inventoryNumber: '',
-                geoLocations: '',//transformation FIX
-                measurements: '',
-                formats: '',
-                year: '',
-                publisher: '',
-                actors: '',
-                places: '',//.def = language
+                subjects: [],
+                // // //legacy_tags: result.legacy_tags,
+                // // //license: result.license,
+                // // //license_id: result.license, //rights
+                // // //license_version: result.license_version,
+                // license_link: '',
+                infoURL: item.isShownAt,
+                inventoryNumber: [],
+                // geoLocations: '',//transformation FIX
+                measurements: [],
+                formats: [],
+                // year: '',
+                // publisher: '',
+                // actors: '',
+                places: [],
                 collection: [],
-                imageRights: '',
-                inscriptions: '',
-                datecreated: '',
-                language: ''
+                // imageRights: '', //!!
+                // inscriptions: '',
+                // datecreated: '',
+                // language: ''
             }
 
             // if (!!item.year && item.year.length > 0) {
             //     image.year = item.year[0];
             // }
 
-            // if (!!item.isShownAt && item.isShownAt.length > 0) {
-            //     image.infoURL = item.isShownAt[0];
-            // }
+            if (!!item.sourceResource.title && item.sourceResource.title.length > 0) {
+                for (let title of item.sourceResource.title) {
+                    image.title.push(title);
+                  }
+            }
 
-            // if (!!item.sourceResource.title && item.sourceResource.title.length > 0) {
-            //     for (let title of item.sourceResource.title) {
-            //         image.title.push(title);
-            //       }
-            // }
+            if (!!item.sourceResource.creator && item.sourceResource.creator.length > 0) {
+                for (let creator of item.sourceResource.creator) {
+                    image.creators.push(creator);
+                }
+            }
 
-            // if (!!item.sourceResource.creator && item.sourceResource.creator.length > 0) {
-            //     for (let creator of item.sourceResource.creator) {
-            //         image.creators.push(creator);
-            //       }
-            // }
+            if (!!item.provider) {
+                image.institutions.push(item.provider.name);
+            }
 
-            // if (!!item.provider.name && item.provider.name.length > 0) {
-            //         image.institutions.push(item.provider.name);
-            // }
+            if (!!item.sourceResource.collection && item.sourceResource.collection.length > 0) {
+                for (let collection of item.sourceResource.collection) {
+                    image.collection.push(collection.title);
+                }
+            }
 
-            // if (!!item.sourceResource.collection.title && item.sourceResource.collection.title.length > 0) {
-            //     for (let title of item.sourceResource.collection.title) {
-            //         image.collection.push(title);
-            //       }
-            // }
+            if (!!item.sourceResource.date && item.sourceResource.date.length > 0) {
+                image.datecreated = item.sourceResource.date[0];
+            }
+
+            if (!!item.sourceResource.description && item.sourceResource.description.length > 0) {
+                image.description = item.sourceResource.description[0];
+            }
+
+            if (!!item.sourceResource.format && item.sourceResource.format.length > 0) {
+                for (let format of item.sourceResource.format) {
+                    image.formats.push(format);
+                }
+            }
+
+            if (!!item.sourceResource.extent && item.sourceResource.extent.length > 0) {
+                for (let extent of item.sourceResource.extent) {
+                    image.measurements.push(extent);
+                }
+            }
+
+            if (!!item.sourceResource.identifier && item.sourceResource.identifier.length > 0) {
+                for (let identifier of item.sourceResource.identifier) {
+                    image.inventoryNumber.push(identifier);
+                }
+            }
+
+            if (!!item.sourceResource.spatial && item.sourceResource.spatial.length > 0) {
+                for (let spatial of item.sourceResource.spatial) {
+                    image.places.push(spatial.name);
+                }
+            }
+
+            if (!!item.sourceResource.subject && item.sourceResource.subject.length > 0) {
+                for (let subject of item.sourceResource.subject) {
+                    image.subjects.push(subject.name);
+                }
+            }
             
-
             images.push(image);
         }
 
